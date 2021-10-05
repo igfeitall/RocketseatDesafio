d7bd313f7e7bc30b37a50657afec02d8bf3090d8
igor feital <igfeitall@gmail.com>
2021-10-05 13:01:56 -0300

    incluindo Yup para validação de schema

--- MeetUp/src/app/controllers/UserController.js
+++ MeetUp/src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from "yup";
 import User from "../models/users";
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: "Validation error. " });
+    }
+
     let userExist = await User.findOne({ where: { email: req.body.email } });
 
     if (userExist) {
@@ -18,6 +29,24 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when("oldPassword", (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: "Validation error. " });
+    }
+
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
 
